de1b45ff7d5789a7b44bf358de331bdee362ff7e
kaleabSolomon <kaleabslmn@gmail.com>
2025-03-09 02:38:32 +0300

    feat(entities): create User entity and establish relationship with Task entity

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Task } from 'src/task/entities/task.entity';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  // One User can have many tasks (One-to-Many Relationship)
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
+}
--- src/task/entities/task.entity.ts
+++ src/task/entities/task.entity.ts
@@ -1,5 +1,6 @@
+import { User } from 'src/auth/entities/user.entity';
 import { TaskStatus } from 'src/common/enums/task';
-import { Column } from 'typeorm';
+import { Column, ManyToOne } from 'typeorm';
 import { CreateDateColumn } from 'typeorm/decorator/columns/CreateDateColumn';
 import { PrimaryGeneratedColumn } from 'typeorm/decorator/columns/PrimaryGeneratedColumn';
 import { Entity } from 'typeorm/decorator/entity/Entity';
@@ -29,4 +30,8 @@ export class Task {
 
   @CreateDateColumn()
   createdAt: Date;
+
+  // Many Tasks belong to one User (Many-to-One Relationship)
+  @ManyToOne(() => User, (user) => user.tasks)
+  user: User;
 }
