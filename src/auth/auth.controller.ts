ac8c1e69a004a56147366a615d88bc84c85e98ee
kaleabSolomon <kaleabslmn@gmail.com>
2025-03-09 03:08:04 +0300

    feat(auth): implement user signup functionality with email validation and token generation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { NoAuth } from 'src/common/decorators';
+import { SignupDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @NoAuth()
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/signup')
+  async localSignup(@Body() dto: SignupDto) {
+    const tokens = await this.authService.signup(dto);
+    return tokens;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { SignupDto } from './dto';
+import * as argon from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private config: ConfigService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(dto: SignupDto) {
+    const { email, password, firstName, lastName } = dto;
+    const user = await this.userRepository.findBy({ email });
+    console.log(user.length > 0);
+    if (user.length > 0)
+      throw new ConflictException(
+        'An Account is already registered with the given email. please Log in or register using a different email',
+      );
+
+    const passwordHash = await this.hashData(password);
+
+    const newUser = this.userRepository.create({
+      email,
+      firstName,
+      lastName,
+      password: passwordHash,
+    });
+    const savedUser = await this.userRepository.save(newUser);
+
+    if (!savedUser) throw new ConflictException('Failed to create user');
+
+    const token = await this.generateTokens(savedUser.id, savedUser.email);
+
+    return token;
+  }
+
+  async hashData(data: string): Promise<string> {
+    return await argon.hash(data);
+  }
+
+  async generateTokens(userId: string, email: string) {
+    try {
+      const at = this.jwtService.sign(
+        { sub: userId, email },
+        {
+          secret: this.config.get('AT_SECRET'),
+          expiresIn: this.config.get('AT_EXPIRESIN'),
+        },
+      );
+
+      return { access_token: at };
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException('Failed to generate tokens');
+    }
+  }
+}
